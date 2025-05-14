1391e2849a9bfe5a3a66238fecb515c0123eef5b
madorTill <mador.till@gmail.com>
2025-05-14 09:10:46 +0300

    add to package btn

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,154 @@
 <template>
-    <div id="app">
-        <img src="@/assets/media/mifkada-logo.png" class="logo" alt="Logo" @click = "homePage">
-        <img v-if="page > 0" src="@/assets/media/graytriangle.png" alt="gray triangle" class="gray-triangle">
-        <button v-show = "page === 0  || page === 2" class = "aboutBtn" @click = "openAbout">i</button>
-        <Transition>
-          <div v-show = "showAbout" class="div-about">
-          <h3 class = "list-text-about">מפתחת ראשית:</h3>
-            <p class = "list-text-about">טור' אדוה אבא</p>
-            <h3 class = "list-text-about">מפתחת משנית:</h3>
-            <p class = "list-text-about">סמל במיל' תמר סטופ</p>
-            <h3 class = "list-text-about">גרפיקה:</h3>
-            <p class = "list-text-about">טור' אדוה אבא</p>
-            <h3 class = "list-text-about">רת"ח מו"פ:</h3>
-            <p class = "list-text-about">רס"ל אביב אואנונו</p>
-            <h3 class = "list-text-about">רמ"ד טי"ל:</h3>
-            <p class = "list-text-about">רס"מ שלומי אוגרן</p>
-            <h3 class = "list-text-about">גרסה:</h3>
-            <p class = "list-text-about">מרץ 2024</p>
-        </div>
-      </Transition>
-        <open-screen v-if="page === 0" @next-page="nextPage"></open-screen>
-        <home-page  v-if="page === 1" @next-page="nextPage"></home-page>
-        <end-screen v-if="page===2"></end-screen>
-        
-        <img src="@/assets/media/madortill.png" alt="mador-till" class="mador-till">
-    </div>
+  <div id="app">
+    <img
+      src="@/assets/media/mifkada-logo.png"
+      class="logo"
+      alt="Logo"
+      @click="homePage"
+    />
+    <img
+      v-if="page > 0"
+      src="@/assets/media/graytriangle.png"
+      alt="gray triangle"
+      class="gray-triangle"
+    />
+    <button
+      v-show="page === 0 || page === 2"
+      class="aboutBtn"
+      @click="openAbout"
+    >
+      i
+    </button>
+    <Transition>
+      <div v-show="showAbout" class="div-about">
+        <h3 class="list-text-about">מפתחת ראשית:</h3>
+        <p class="list-text-about">טור' אדוה אבא</p>
+        <h3 class="list-text-about">מפתחת משנית:</h3>
+        <p class="list-text-about">סמל במיל' תמר סטופ</p>
+        <h3 class="list-text-about">גרפיקה:</h3>
+        <p class="list-text-about">טור' אדוה אבא</p>
+        <h3 class="list-text-about">רת"ח מו"פ:</h3>
+        <p class="list-text-about">רס"ל אביב אואנונו</p>
+        <h3 class="list-text-about">רמ"ד טי"ל:</h3>
+        <p class="list-text-about">רס"מ שלומי אוגרן</p>
+        <h3 class="list-text-about">גרסה:</h3>
+        <p class="list-text-about">מרץ 2024</p>
+      </div>
+    </Transition>
+    <open-screen v-if="page === 0" @next-page="nextPage"></open-screen>
+    <home-page v-if="page === 1" @next-page="nextPage"></home-page>
+    <end-screen v-if="page === 2"></end-screen>
+
+    <img
+      src="@/assets/media/madortill.png"
+      alt="mador-till"
+      class="mador-till"
+    />
+    <div v-if="page === 0" class="toPackageBtn" @click="toPackage">חזרה למארז</div>
+  </div>
 </template>
 
 <script>
-
-import OpenScreen from '@/components/OpenScreen.vue';
-import HomePage from '@/components/HomePage.vue';
-import EndScreen from '@/components/EndScreen.vue';
+import OpenScreen from "@/components/OpenScreen.vue";
+import HomePage from "@/components/HomePage.vue";
+import EndScreen from "@/components/EndScreen.vue";
 
 export default {
-    name: "app",
-    components: {
-        OpenScreen,
-        HomePage,
-        EndScreen,
+  name: "app",
+  components: {
+    OpenScreen,
+    HomePage,
+    EndScreen,
+  },
+  data() {
+    return {
+      page: 0,
+      showAbout: false,
+      clickBtn: 0,
+    };
+  },
+  methods: {
+    nextPage() {
+      this.page++;
     },
-    data() {
-        return {
-            page: 0 ,
-            showAbout: false,
-            clickBtn: 0,
-        };
+    prevPage() {
+      this.page--;
     },
-    methods: {
-        nextPage() {
-            this.page++;
-        },
-        prevPage() {
-            this.page--;
-        },
-        homePage() {
-            thispage === 1;
-        },
-        openAbout() {
-          if (this.clickBtn % 2 === 0) {
-                this.showAbout = true;
-            } else {
-                this.showAbout = false
-            }
-            this.clickBtn++;
-        }
+    homePage() {
+      thispage === 1;
     },
+    openAbout() {
+      if (this.clickBtn % 2 === 0) {
+        this.showAbout = true;
+      } else {
+        this.showAbout = false;
+      }
+      this.clickBtn++;
+    },
+    toPackage() {
+      window.location.href = "https://madortill.github.io/MentoringPackage/";
+    },
+  },
 };
-
 </script>
 
 <style>
 @font-face {
-    font-family: 'Heebo';
-    src: url('@/assets/fonts/heebo.regular.ttf');
+  font-family: "Heebo";
+  src: url("@/assets/fonts/heebo.regular.ttf");
 }
 
 @font-face {
-    font-family: 'Heebo-Bold';
-    src: url('@/assets/fonts/heebo.black.ttf');
+  font-family: "Heebo-Bold";
+  src: url("@/assets/fonts/heebo.black.ttf");
 }
 
 * {
-    overflow: hidden;
-    font-family: 'Heebo';
+  overflow: hidden;
+  font-family: "Heebo";
 }
 
 body {
-    margin: 0;
-    direction: rtl;
+  margin: 0;
+  direction: rtl;
 }
 
 #app {
-    font-family: 'Heebo';
-    width: 100vw;
-    height: 100vh;
-    position: relative;
+  font-family: "Heebo";
+  width: 100vw;
+  height: 100vh;
+  position: relative;
 }
 
 .logo {
-    max-width: 100px;
-    left: 0;
-    top: 0;
-    position: absolute;
-    margin-top: 1%;
-    margin-left: 1%;
-    z-index: 3;
+  max-width: 100px;
+  left: 0;
+  top: 0;
+  position: absolute;
+  margin-top: 1%;
+  margin-left: 1%;
+  z-index: 3;
 }
 
 .mador-till {
-    /* min-width: 20px; */
-    max-height: 10%;
-    max-width: 90px;
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    margin-bottom: 1%;
-    margin-right: 1%;
-    z-index: 3;
+  /* min-width: 20px; */
+  max-height: 10%;
+  max-width: 90px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 1%;
+  margin-right: 1%;
+  z-index: 3;
 }
 
 .gray-triangle {
-    min-width: 50px;
-    max-width: 220px;
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    z-index: 2;
+  min-width: 50px;
+  max-width: 220px;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
 }
 
 .aboutBtn {
@@ -142,10 +164,9 @@ body {
   cursor: pointer;
   left: 2.5%;
   top: 16%;
-
 }
 
-.aboutBtn:hover{
+.aboutBtn:hover {
   background-color: #038eb9;
 }
 .div-about {
@@ -167,6 +188,26 @@ body {
 .list-text-about:hover {
   background-color: #dbdbdb;
 }
-</style> 
+.toPackageBtn {
+  background-image: url(@/assets/media/circle.svg);
+  background-size: 100% 100%;
+  height: 7rem;
+  width: 7rem;
+  text-align: center;
+  align-content: center;
+  color: #ebebeb;
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  filter: drop-shadow(0 5px 7px rgba(0, 0, 0, 0.2));
+  animation-play-state: paused;
+  font-size: 1.2rem;
+}
 
-  
+.toPackageBtn:hover {
+  height: 8rem;
+  width: 8rem;
+}
+</style>
--- src/components/EndScreen.vue
+++ src/components/EndScreen.vue
@@ -1,177 +1,186 @@
 <template>
-    <div id="end-screen">
-        <h3 class="conclusion-text">לסיכום</h3>
-        <div class="finale-exe type-writer">
-            <p v-for="(text, index) in array1" :key="text" :style="{
-                '--delay': `${index * 3.2}s`,
-                '--width': `${text.length}ch`,
-                'font-size': index === 0 || index === 2 ? '3rem' : '1.6rem'
-            }" class = "text-writer">
-            {{ text }}
-             <!-- <div v-show = "index != 0 && index != 2" class = "check-list">
+  <div id="end-screen">
+    <h3 class="conclusion-text">לסיכום</h3>
+    <div class="finale-exe type-writer">
+      <p
+        v-for="(text, index) in array1"
+        :key="text"
+        :style="{
+          '--delay': `${index * 3.2}s`,
+          '--width': `${text.length}ch`,
+          'font-size': index === 0 || index === 2 ? '3rem' : '1.6rem',
+        }"
+        class="text-writer"
+      >
+        {{ text }}
+        <!-- <div v-show = "index != 0 && index != 2" class = "check-list">
                 <svg class="check-svg" width="70" height="70" viewBox="0 0 70 70">
                     <polyline class="check-poly" points="23 34 34 43 47 27" stroke-linecap="round" />
                 </svg>
             </div>  -->
-        </p>
-        </div>
-
-        <img v-show="showClose" src = "@/assets/media/שרוך-בורדו-מעודכן.svg" class = "burgundy-lace">
-        <div v-show="showClose" class="finish-exe">
-            <h1 class="finale-text">כל הכבוד סיימת את לומדת המשוב!</h1>
-            <button @click="closeWindow" class="closeBtn">סיום</button>
-        </div>
+      </p>
     </div>
+
+    <img
+      v-show="showClose"
+      src="@/assets/media/שרוך-בורדו-מעודכן.svg"
+      class="burgundy-lace"
+    />
+    <div v-show="showClose" class="finish-exe">
+      <h1 class="finale-text">כל הכבוד סיימת את לומדת המשוב!</h1>
+      <button @click="closeWindow" class="closeBtn">סיום</button>
+    </div>
+    <div v-if="showClose" class="toPackageBtn" @click="toPackage">חזרה למארז</div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'end-screen',
-    components: {
-
+  name: "end-screen",
+  components: {},
+  data() {
+    return {
+      showClose: false,
+      array1: [
+        "מטרת על",
+        "החניך יעביר משוב אישי בצורה אפקטיבית.",
+        "מטרות ביניים",
+        "החניך יפרט עקרונות יסוד בשלב המשוב.",
+        " החניך יפרט את הכללים והמבנה הנכון של שיחת המשוב. ",
+        "החניך יסביר את תהליך איתור מקורות אפשריים לתופעות במשוב.",
+        "החניך יפרט את תהליך התמודדות עם התנגדויות במשוב.",
+      ],
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showClose = true;
+    }, 23500);
+  },
+  methods: {
+    closeWindow() {
+      window.close(); // סוגר את חלון האתר.
     },
-    data() {
-        return {
-            showClose: false,
-            array1: [
-                'מטרת על',
-                'החניך יעביר משוב אישי בצורה אפקטיבית.',
-                'מטרות ביניים',
-                'החניך יפרט עקרונות יסוד בשלב המשוב.',
-                ' החניך יפרט את הכללים והמבנה הנכון של שיחת המשוב. ',
-                'החניך יסביר את תהליך איתור מקורות אפשריים לתופעות במשוב.',
-                'החניך יפרט את תהליך התמודדות עם התנגדויות במשוב.'
-            ],
-        };
+    toPackage() {
+      window.location.href = "https://madortill.github.io/MentoringPackage/";
     },
-    mounted() {
-        setTimeout(() => {
-            this.showClose = true;
-        }, 23500);
-    },
-    methods: {
-        closeWindow() {
-            window.close(); // סוגר את חלון האתר.
-        },
-    },
-}
+  },
+};
 </script>
 
 <style>
-
 #end-screen {
-    background-color: #F2F2F2;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    position: relative;
-    justify-content: center;
-    counter-reset: lines;
+  background-color: #f2f2f2;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  justify-content: center;
+  counter-reset: lines;
 }
 
 .finale-exe {
-    position: absolute;
-    width: 40%;
-    height: 70%;
-    /* left: 30%; */
-    bottom: 20%;
-    background: #fff;
-    border-radius: 50px;
-    box-shadow: 0 15px 20px -20px rgba(0, 0, 0, 0.4);
-    text-align: center;
-    padding: 0 2rem;
+  position: absolute;
+  width: 40%;
+  height: 70%;
+  /* left: 30%; */
+  bottom: 20%;
+  background: #fff;
+  border-radius: 50px;
+  box-shadow: 0 15px 20px -20px rgba(0, 0, 0, 0.4);
+  text-align: center;
+  padding: 0 2rem;
 }
 
-.type-writer>p {
-    animation: typeWrite 2s var(--delay) steps(44) normal both, blink 400ms var(--delay) steps(45) 6;
+.type-writer > p {
+  animation: typeWrite 2s var(--delay) steps(44) normal both,
+    blink 400ms var(--delay) steps(45) 6;
 }
 .finish-exe {
-    position: absolute;
-    width: 100%;
-    height: 50%;
-    text-align: center;
-    background: #1F4E79;
-    bottom: 30%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  text-align: center;
+  background: #1f4e79;
+  bottom: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .finale-text {
-    color: white;
-    font-size: 3.5rem;
+  color: white;
+  font-size: 3.5rem;
 }
 
 .conclusion-text {
-    color: #5f5a5a;
-    font-size: 1.7rem;
-    position: fixed;
+  color: #5f5a5a;
+  font-size: 1.7rem;
+  position: fixed;
 }
 
 @keyframes typeWrite {
-    from {
-        width: 0%;
-    }
+  from {
+    width: 0%;
+  }
 
-    to {
-        width: var(--width);
-    }
+  to {
+    width: var(--width);
+  }
 }
 
-.finale-exe>p {
-    display: block;
-    width: fit-content;
-    white-space: nowrap;
-    text-align: right;
-    overflow: hidden;
-    border-left: solid 3px transparent;
-    max-width: fit-content;
-    margin: 4%;
+.finale-exe > p {
+  display: block;
+  width: fit-content;
+  white-space: nowrap;
+  text-align: right;
+  overflow: hidden;
+  border-left: solid 3px transparent;
+  max-width: fit-content;
+  margin: 4%;
 }
-
 
 @keyframes blink {
-    to {
-        border-left-color: black;
-    }
+  to {
+    border-left-color: black;
+  }
 
-    from {
-        border-left-color: transparent;
-    }
+  from {
+    border-left-color: transparent;
+  }
 }
 
 .closeBtn {
-    position: absolute;
-    border: none;
-    cursor: pointer;
-    font-size: 1.6rem;
-    color: #ffffff;
-    border-radius: 100px;
-    background-color: #0492bd;
-    min-width: 10%;
-    height: 10%;
-    top: 70%;
+  position: absolute;
+  border: none;
+  cursor: pointer;
+  font-size: 1.6rem;
+  color: #ffffff;
+  border-radius: 100px;
+  background-color: #0492bd;
+  min-width: 10%;
+  height: 10%;
+  top: 70%;
 }
 
 .closeBtn:hover {
-    animation: borderPulse 4000ms infinite ease-out, hoverShine 200ms;
+  animation: borderPulse 4000ms infinite ease-out, hoverShine 200ms;
 }
 
 .about-div {
-    color: #F2F2F2;
-    position: absolute;
-    left: 5%;
-    bottom: 5%;
+  color: #f2f2f2;
+  position: absolute;
+  left: 5%;
+  bottom: 5%;
 }
 
 .burgundy-lace {
-    position: relative;
-    width: 36%;
-    /* top: 0%; */
-    bottom: 11%;
-    right: 20%;
-    z-index: 1;
+  position: relative;
+  width: 36%;
+  /* top: 0%; */
+  bottom: 11%;
+  right: 20%;
+  z-index: 1;
 }
 
 @keyframes expand {
@@ -190,11 +199,10 @@ export default {
   display: inline-block;
   right: -2%;
   /* top :0%; */
-  transform-origin: center;  
+  transform-origin: center;
   animation: 1s cubic-bezier(0.77, 0, 0.175, 1) var(--delay) 1 both scaleCheck;
   /* animation: name duration timing-function delay iteration-count direction fill-mode; */
 }
-
 
 .check-poly {
   fill: transparent;
@@ -202,7 +210,7 @@ export default {
   stroke-dasharray: 36px 36px;
   stroke: #3374ff;
   animation: 1s cubic-bezier(0.77, 0, 0.175, 1) var(--delay) 1 both drawCheck;
- }
+}
 
 @keyframes scaleCheck {
   0% {
@@ -213,7 +221,7 @@ export default {
   }
   100% {
     transform: scale(1);
-  } 
+  }
 }
 
 @keyframes drawCheck {
@@ -226,11 +234,34 @@ export default {
 }
 
 .check-list {
-    position: relative;
-    /* margin-top: 5%; */
+  position: relative;
+  /* margin-top: 5%; */
 }
 
 .text-writer {
-    /* position: relative; */
+  /* position: relative; */
+}
+
+.toPackageBtn {
+  background-image: url(@/assets/media/circle.svg);
+  background-size: 100% 100%;
+  height: 7rem;
+  width: 7rem;
+  text-align: center;
+  align-content: center;
+  color: #ebebeb;
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  filter: drop-shadow(0 5px 7px rgba(0, 0, 0, 0.2));
+  animation-play-state: paused;
+  font-size: 1.2rem;
+}
+
+.toPackageBtn:hover {
+  height: 8rem;
+  width: 8rem;
 }
 </style>
